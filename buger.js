a3b91c9a6caef9c5b11684310e2c83726027ddd6
xrr2016 <xiaoranran1993@163.com>
2017-10-08 03:44:01 +0800

    :art:

--- buger.js
+++ buger.js
@@ -9,10 +9,33 @@
 
     }
     render () {
-
+      const temp = `<div class=""></div>`
     }
     listener () {
       
     }
   }
 })()
+
+const cb = function () { console.log('toggle open') }
+
+const container = document.querySelector('.container')
+
+container.addEventListener('click', (e) => {
+  container.classList.toggle('open')
+  cb.call(this)
+}, false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
